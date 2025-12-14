3ece52f52b0bc64b0326cba9abe8dcb10fdd4fa0
Антонов Егор Андреевич <EAntonov8@alfabank.ru>
2025-12-14 17:05:41 +0300

    feat: update run

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { filePath } from "./constants/path.js";
 import { readFromFile } from "./utils/read-from-file.js";
 
-const main = () => {
-  const martrix = readFromFile(filePath);
+const main = async () => {
+  const matrix = await readFromFile(filePath);
+  console.log(matrix);
 };
 
 main();
