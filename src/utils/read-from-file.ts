a799da13ef6f7c3e3e3691704ef4e3709d0a4562
Антонов Егор Андреевич <EAntonov8@alfabank.ru>
2025-12-14 17:35:27 +0300

    feat:create correct parse elements

--- src/utils/read-from-file.ts
+++ src/utils/read-from-file.ts
@@ -1,11 +1,35 @@
 import { readFile } from "fs/promises";
 import { join } from "path";
 
-export const readFromFile = async (filePath: string): Promise<string[][]> => {
-  const content = await readFile(filePath, "utf-8");
-  const lines = content.trim().split("\n");
+type readFromFileType = {
+  vertexCount: number;
+  edgeCount: number;
+  edges: number[][];
+};
 
-  return lines
-    .filter((line) => line.trim().length > 0)
-    .map((line) => line.trim().split(/\s+/));
+export const readFromFile = async (
+  filePath: string
+): Promise<readFromFileType> => {
+  const content = await readFile(filePath, "utf-8");
+  const lines: string[][] = content
+    .trim()
+    .split("\n")
+    .map((el) => el.split(" "));
+
+  const firstLine = lines[0];
+  if (!firstLine || firstLine.length < 2) {
+    throw new Error("First line must contain n and m");
+  }
+
+  const vertexCountStr = firstLine[0];
+  const edgeCountStr = firstLine[1];
+  if (!vertexCountStr || !edgeCountStr) {
+    throw new Error("First line must contain n and m");
+  }
+
+  return {
+    vertexCount: Number(vertexCountStr),
+    edgeCount: Number(edgeCountStr),
+    edges: lines.slice(1).map((el) => el.map((item) => Number(item))),
+  };
 };
